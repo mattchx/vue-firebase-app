be12614573596d221449d939c2e73094b741b167
Matt <mattcharlesh@gmail.com>
2021-02-10 21:22:47 -0500

    corrected main

--- src/main.js
+++ src/main.js
@@ -7,13 +7,19 @@ import './assets/scss/app.scss'
 
 Vue.config.productionTip = false
 
+
+
 let app
-auth.onAuthStateChanged(() => {
+auth.onAuthStateChanged(user => {
   if (!app) {
     app = new Vue({
       router,
       store,
       render: h => h(App)
     }).$mount('#app')
+  }
+
+  if (user) {
+    store.dispatch('fetchUserProfile', user)
   }
 })
